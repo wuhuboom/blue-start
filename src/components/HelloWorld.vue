149bfe6b0dad9b866a813b6512f2e85c47e98787
attoote <119163844+attoote@users.noreply.github.com>
2024-03-02 20:25:38 +0700

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,4 +9,12 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.page-load {
+  height: 100vh;
+  background: url("@/assets/img/bg.webp") no-repeat center top;
+  background-size: cover;
+  max-width: 750px;
+  margin: 0 auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import "@/assets/css/comm.less";
 Vue.config.productionTip = false;
 
 new Vue({
